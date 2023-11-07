691c59101e382f7e1c6e6557f84a3d2608ce4348
Hilária Saraiva Pereira <145464554+hilariasp@users.noreply.github.com>
2023-11-07 09:33:39 -0300

    Add files via upload

---
+++ aula01/exp07.php
@@ -0,0 +1,15 @@
+<?php
+print "<h2>PHP is fun</h2>";
+print "Hello World<br>";
+print "I'm about to learn PHP!<br>";
+print "This"." string"." was"." made"." with multiple parameters";
+
+$txt1 = "Hello World";
+$txt2 = "ifbaiano";
+$x = 5;
+$y = 4;
+
+print "<h2>" . $txt1 . "</h2>";
+print "Study PHP " . $txt2 ."<br>";
+print $x + $y;
+?>
