7b6b19ff9d1947fb9f00c6db0240f42ded9f0734
Hilária Saraiva Pereira <145464554+hilariasp@users.noreply.github.com>
2024-07-09 11:23:15 -0300

    Add files via upload

---
+++ aula_09-07/exemplo-include/conteudo.php
@@ -0,0 +1,17 @@
+<?php 
+include_once("header.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Conteúdo</p>
+</body>
+</html>
+<?php 
+include_once("footer.php");
+?>
